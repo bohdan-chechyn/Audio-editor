e9dcf8657ab6a5f8c72c94374cc1e2df7e6708f1
Bohdan Chechin <b.chechin@2gis.kiev.ua>
2012-02-25 22:30:09 +0200

    tools

---
+++ js/VisualSample.js
@@ -0,0 +1,33 @@
+var VisualSample = function(options) {
+    this.init(options);
+}
+VisualSample.prototype = {
+    container: null,
+    init: function(options) {
+        l(options);
+        var container = $('#sample-template').html()
+        .replace('${width}', options.width)
+        .replace('${offset}', options.offset);
+        container = $(container);
+        container.id = options.sampleId;
+        container.data('sample', options.sample);
+        container.bind('mouseover', this.showTools);
+        container.bind('mouseout', this.hideTools);
+        container.find('.close').bind('click', this.destroy);
+        this.container = container;
+    },
+    
+    showTools: function() {
+        $(this).find('.tools').show();
+    },
+    
+    hideTools: function() {
+        $(this).find('.tools').hide();
+    },
+    
+    destroy: function() {
+        $(this).parents('.sample').fadeOut(600, function(){
+            $(this).remove();
+        });
+    }
+}
